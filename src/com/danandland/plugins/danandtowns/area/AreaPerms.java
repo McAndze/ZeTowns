d78580dcbe6295852c5644891fc9f5f783951939
Huliheaden <luniadk@gmail.com>
2012-01-26 16:17:12 +0100

    initial code for power and area permissions

---
+++ src/com/danandland/plugins/danandtowns/area/AreaPerms.java
@@ -0,0 +1,41 @@
+package com.danandland.plugins.danandtowns.area;
+
+import java.util.HashMap;
+
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+
+public class AreaPerms {
+	public enum Permission{
+		BUILD, DESTROY, ITEM, SWITCH
+	}
+	
+	// f = friend/resident | r = resident/friend | a = ally | o = outsider | p = 
+	private String buildPerms;
+	private String destroyPerms;
+	private String itemPerms;
+	private String switchPerms;
+	
+	
+	
+	
+	/*
+	 * STATIC
+	 */
+	// f = friend
+	
+	public static HashMap<World, HashMap<AreaPerms.Permission, Boolean>> worldOverrides = 
+		new HashMap<World, HashMap<AreaPerms.Permission, Boolean>>();
+	
+	/**
+	 * TESTING
+	 */
+	public static void initialize(){
+		World world = Bukkit.getServer().getWorld("world");
+		HashMap<AreaPerms.Permission, Boolean> map = new HashMap<AreaPerms.Permission, Boolean>();
+		map.put(AreaPerms.Permission.SWITCH, new Boolean(true));
+	}
+	/**
+	 * TESTING\
+	 */
+}
